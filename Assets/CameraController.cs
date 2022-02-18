345e5a7db9f3ba334aae3e60a3964b4fcacf34d8
mjpurin <mjpurin@gmail.com>
2022-02-19 08:44:35 +0900

    Unity9の31まで作成

---
+++ Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    void LateUpdate()
+    {
+        /*
+        transform.position = 
+            player.position + (-player.forward * 3.0f)+(player.up*1.0f);
+        */
+        transform.position=
+            Vector3.Lerp(transform.position,
+            player.position+(-player.forward*3.0f)
+            +(player.up*1f),Time.deltaTime*10f);
+
+        transform.LookAt(player.position + Vector3.up);
+
+        
+    }
+}
